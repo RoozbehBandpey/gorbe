8428ebc3b39bf5331f6aa225ac9c823d4391827d
RoozbehBandpey <r.bandpey67@gmail.com>
2022-11-13 22:02:25 +0100

    generate some files

--- di/src/computer/computer.module.ts
+++ di/src/computer/computer.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ComputerService } from './computer.service';
+import { ComputerController } from './computer.controller';
 
-@Module({})
+@Module({
+  providers: [ComputerService],
+  controllers: [ComputerController]
+})
 export class ComputerModule {}
