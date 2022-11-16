7a84882923e6474e9420fbede85119a00ebe5b1b
RoozbehBandpey <r.bandpey67@gmail.com>
2022-11-16 20:05:27 +0100

    updating user

--- my-car-value/src/users/users.service.ts
+++ my-car-value/src/users/users.service.ts
@@ -19,6 +19,19 @@ export class UsersService {
   find(email: string) {
     return this.repo.find({ where: { email } });
   }
-  update() {}
-  remove() {}
+  async update(id: number, attrs: Partial<User>) {
+    // This is a little bit inefficient since we do two trips
+    // to database but that's the only way to have nice functionality if logging
+    // Efficient way is to directly call update() which we'll loose logging on it
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    Object.assign(user, attrs);
+    
+    return this.repo.save(user);
+  }
+  remove(id: number) {
+
+  }
 }
