062d52ecc46d367f42cb6afd3173fc716c72bb77
RoozbehBandpey <r.bandpey67@gmail.com>
2022-11-16 20:10:25 +0100

    removing users

--- my-car-value/src/users/users.service.ts
+++ my-car-value/src/users/users.service.ts
@@ -13,25 +13,37 @@ export class UsersService {
 
     return this.repo.save(user);
   }
+
   findOne(id: number) {
     return this.repo.findOneBy({ id })
   }
+
   find(email: string) {
     return this.repo.find({ where: { email } });
   }
+
   async update(id: number, attrs: Partial<User>) {
     // This is a little bit inefficient since we do two trips
     // to database but that's the only way to have nice functionality if logging
     // Efficient way is to directly call update() which we'll loose logging on it
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('User not found');
+      throw new Error('User not found!');
     }
     Object.assign(user, attrs);
-    
+
     return this.repo.save(user);
   }
-  remove(id: number) {
 
+  async remove(id: number) {
+    //  Same as above we cn do remove()  or delete(), 
+    // delete is faster but no hooks associated with entity would work
+    // Remove works with entities
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found!');
+    }
+
+    return this.repo.remove(user);
   }
 }
