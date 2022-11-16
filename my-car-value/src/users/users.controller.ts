dfc1c3b0c4865fbb2d30d6d3e9423be42dd17f15
RoozbehBandpey <r.bandpey67@gmail.com>
2022-11-16 20:22:43 +0100

    removing users

--- my-car-value/src/users/users.controller.ts
+++ my-car-value/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Get, Patch, Param, Query } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Query, Delete } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -22,4 +22,8 @@ export class UsersController {
   }
 
   
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
 }
