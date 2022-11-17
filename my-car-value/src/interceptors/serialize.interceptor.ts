a3b3e39d939300724613e402ed858bbbb4195f12
RoozbehBandpey <r.bandpey67@gmail.com>
2022-11-17 22:01:05 +0100

    serialization

--- my-car-value/src/interceptors/serialize.interceptor.ts
+++ my-car-value/src/interceptors/serialize.interceptor.ts
@@ -7,16 +7,18 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
+import { UserDto } from 'src/users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
   intercept( context: ExecutionContext, handler: CallHandler<any> ): Observable<any> | Promise<Observable<any>> {
     // Run something before a request is handled by the request handler
-    console.log(context);
 
     return handler.handle().pipe(
       map((data: any) => {
         // Run something before the response is sent out
-        console.log(data);
+        return plainToClass(UserDto, data, {
+          excludeExtraneousValues: true
+        });
       })
     );
   }
