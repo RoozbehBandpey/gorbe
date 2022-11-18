c3058392cea4b627e635f64d6fadfe8d22d10a26
RoozbehBandpey <r.bandpey67@gmail.com>
2022-11-18 22:16:53 +0100

    salting and hashing passwords

--- my-car-value/src/users/auth.service.ts
+++ my-car-value/src/users/auth.service.ts
@@ -1,5 +1,9 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
 
 export class AuthService {
   constructor(private usersService: UsersService) {}
@@ -11,7 +15,12 @@ export class AuthService {
       throw new BadRequestException('Email already in use!')
     }
     // Hash user's password
-
+    // Generate a salt
+    const salt = randomBytes(8).toString('hex'); // 16 characters, each byte is two characters
+    // Hash the salt and the password together
+    const hash = (await scrypt(password, salt, 32)) as Buffer; // 32 bytes as the output
+    // Join the hash result and salt together
+    const result = salt + '.' + hash.toString('hex');
     // Create a new user and save it
 
     // return the user
