93355bb26ccead1ec99beef20d582dabf37b2242
RoozbehBandpey <r.bandpey67@gmail.com>
2022-11-20 20:01:46 +0100

    globally scoped interceptors

--- my-car-value/src/users/users.controller.ts
+++ my-car-value/src/users/users.controller.ts
@@ -8,8 +8,7 @@ import {
   Query,
   Delete,
   Session,
-  NotFoundException, 
-  UseInterceptors} from '@nestjs/common';
+  NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -17,27 +16,21 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from 'src/users/dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 import { User } from './users.entity';
 
 @Serialize(UserDto)
 @Controller('auth')
-@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(
     private usersService: UsersService,
     private authService: AuthService
     ) {}
 
-  // @Get('/whoami')
-  // async whoAmI(@Session() session: any) {
-  //   return this.usersService.findOne(session.userId);
-  // }
-
   @Get('/whoami')
   async whoAmI(@CurrentUser() user: User) {
     return user;
   }
+
   @Post('/signup')
   async createUser(@Body() body: CreateUserDto, @Session() session: any) {
     const user = await this.authService.signup(body.email, body.password);
@@ -71,7 +64,6 @@ export class UsersController {
     return this.usersService.find(email);
   }
 
-  
   @Delete('/:id')
   removeUser(@Param('id') id: string) {
     return this.usersService.remove(parseInt(id));
