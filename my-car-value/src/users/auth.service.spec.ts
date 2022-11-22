f32a0e0d13fee033870231aee92b3f73644a6e2f
RoozbehBandpey <r.bandpey67@gmail.com>
2022-11-22 21:36:01 +0100

    mock improvment

--- my-car-value/src/users/auth.service.spec.ts
+++ my-car-value/src/users/auth.service.spec.ts
@@ -2,13 +2,15 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './users.entity';
+import { BadRequestException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
   
   beforeEach(async () => {
     // Create a fake copy of user service
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) => Promise.resolve({ id: 1, email, password } as User)
     }
@@ -39,4 +41,39 @@ describe('AuthService', () => {
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
   });
+
+  it('throws an error if the user with email that is in use', async () => {
+    fakeUsersService.find = () => Promise.resolve([{ id: 1, email: '', password: ''} as User])
+    const  password = '123';
+
+    await expect(service.signup('test@test.com', password)).rejects.toThrow(
+      BadRequestException,
+    );
+  });
 })
+
+
+// Refactor 'Email in Use' Test to Address Jest Breaking Changes
+// Due to breaking changes in the Jest library, we will need to modify our tests to avoid the following errors:
+
+// Test functions cannot both take a 'done' callback and return something. Either use a 'done' callback, or return a promise.
+
+// This was first raised in the QA here.
+
+// Find the src/users/auth.service.spec.ts file and make the following changes:
+
+// 1) Add Import:
+
+// import { BadRequestException } from '@nestjs/common';
+
+
+// 2) Refactor 'throws an error if user signs up' test to remove try/catch and done callback:
+
+//   it('throws an error if user signs up with email that is in use', async () => {
+//     fakeUsersService.find = () =>
+ 
+//       Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+//     await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
+//       BadRequestException,
+//     );
+//   });
