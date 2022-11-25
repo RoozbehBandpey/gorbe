a953c844c9920b23437b9068e12ffa9b0fe32298
RoozbehBandpey <r.bandpey67@gmail.com>
2022-11-25 20:18:03 +0100

    applying globally scoped pipe

--- my-car-value/src/app.module.ts
+++ my-car-value/src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -15,6 +16,14 @@ import { Report } from './reports/reports.entity';
     synchronize: true
   }), UsersModule, ReportsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true
+      })
+    }
+  ],
 })
 export class AppModule {}
