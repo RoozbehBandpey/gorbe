e62f697311777e208aa6377c3ec87817e36223e1
RoozbehBandpey <r.bandpey67@gmail.com>
2022-11-30 23:44:48 +0100

    transforming query string data

--- my-car-value/src/reports/dtos/get-estimate.dto.ts
+++ my-car-value/src/reports/dtos/get-estimate.dto.ts
@@ -6,6 +6,7 @@ import {
   IsLongitude,
   IsLatitude
 } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class GetEstimateDto {
   @IsString()
@@ -14,17 +15,21 @@ export class GetEstimateDto {
   @IsString()
   model: string;
 
+  @Transform(({ value }) => parseInt(value))
   @IsNumber()
   @Min(1930)
   @Max(2050)
   year: number;
 
+  @Transform(({ value }) => parseFloat(value))
   @IsLongitude()
   lng: number;
 
+  @Transform(({ value }) => parseFloat(value))
   @IsLatitude()
   lat: number;
 
+  @Transform(({ value }) => parseInt(value))
   @IsNumber()
   @Min(0)
   @Max(1000000)
